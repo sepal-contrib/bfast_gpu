c2c75a4bffd4822bf97eae12906bcbebca88cefa
Pierrick Rambaud <pierrick.rambaud49@gmail.com>
2021-04-21 12:56:06 +0000

    set the warnings for monitoring

--- component/tile/bfast_tile.py
+++ component/tile/bfast_tile.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from datetime import datetime
 
 import ipyvuetify as v
 from sepal_ui import sepalwidgets as sw 
@@ -44,6 +45,8 @@ class BfastTile(sw.Tile):
         # add js behaviour 
         self.folder.observe(self._on_folder_change, 'v_model')
         self.btn.on_event('click', self._start_process)
+        self.monitoring.observe(self._check_periods, 'v_model')
+        self.history.observe(self._check_periods, 'v_model')
         
     def _start_process(self, widget, event, data):
         """start the bfast process"""
@@ -74,8 +77,16 @@ class BfastTile(sw.Tile):
         if not self.output.check_input(level, cm.widget.level.no_level): return widget.toggle_loading()
         if not self.output.check_input(backend, cm.widget.backend.no_backend): return widget.toggle_loading()
         if not self.output.check_input(len(monitoring), cm.widget.monitoring.no_dates): return widget.toggle_loading()
-        if not self.output.check_input(history, cm.widget.history.no_date): return widget.toggle_loading()       
+        if not self.output.check_input(history, cm.widget.history.no_date): return widget.toggle_loading()  
         
+        # check the dates 
+        start_history = datetime.strptime(history, "%Y-%m-%d")
+        start_monitor = datetime.strptime(moitoring[0], "%Y-%m-%d")
+        end_monitor = datetime.strptime(moitoring[1], "%Y-%m-%d")
+        
+        if not (start_history < start_monitor < end_monitor):
+            self.output.add_msg(cm.widget.monitoring.bad_order, 'error')
+            return widget.toggle_loading()
         
         widget.toggle_loading()
         
@@ -122,5 +133,25 @@ class BfastTile(sw.Tile):
         self.output.add_msg(cm.widget.folder.valid_ts.format(folder))
         
         return self
+    
+    def _check_periods(self, change):
+        """check if the historical period have enough images"""
+        
+        # to avoid bug on disable
+        if not self.history.dates:
+            return self
+        
+        # get the index of the current history and monitoring dates
+        history = self.history.slider.v_model
+        monitor = self.monitoring.range.v_model[0]
+        
+        if history > (monitor-40):
+            self.output.add_msg(cm.widget.history.too_short, 'warning')
+        else:
+            self.output.reset()
+            
+        return self
+        
+        
         
         
