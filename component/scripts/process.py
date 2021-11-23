6cb9412081eb2b82efa84582119ad9d6d2f9161b
12rambau <pierrick.rambaud49@gmail.com>
2021-11-23 20:57:39 +0000

    fix bug preventing from map to load

--- component/scripts/process.py
+++ component/scripts/process.py
@@ -15,225 +15,263 @@ from osgeo import gdal
 from component import parameter as cp
 from component.message import cm
 
+
 def debug(data, dates, segment_dir, loc_monitor_params, write_lock):
-    
+
     with write_lock:
-        # set the debugging folder 
-        debug_dir = cp.result_dir/'bfast_gpu_debug'
+        # set the debugging folder
+        debug_dir = cp.result_dir / "bfast_gpu_debug"
         debug_dir.mkdir(exist_ok=True)
 
-        # extract the time serie name and create a folder 
-        ts_dir = debug_dir/segment_dir.parts[-2]
+        # extract the time serie name and create a folder
+        ts_dir = debug_dir / segment_dir.parts[-2]
         ts_dir.mkdir(exist_ok=True)
 
-        # extract the tile number 
-        tile_dir = ts_dir/segment_dir.parts[-1]
+        # extract the tile number
+        tile_dir = ts_dir / segment_dir.parts[-1]
         tile_dir.mkdir(exist_ok=True)
-    
-    # get the number of folder in the tile and create a new one 
+
+    # get the number of folder in the tile and create a new one
     number = len([f for f in tile_dir.iterdir() if f.is_dir()])
-    save_folder = tile_dir/f'{number}'
+    save_folder = tile_dir / f"{number}"
     save_folder.mkdir()
-    
+
     # save monitoring parameters
-    moitor_params = {k: v if type(v) != dt else v.strftime("%Y-%m-%d") for k, v in loc_monitor_params.items()}
-    (save_folder/'monitor_params.json').write_text(json.dumps(moitor_params, indent=4))
-    
-    # save dates 
-    (save_folder/'dates.json').write_text(json.dumps([d.strftime("%Y-%m-%d") for d in dates], indent=4))
-    
-    # save data 
-    np.save((save_folder/'data.npy'), data)
-    
+    moitor_params = {
+        k: v if type(v) != dt else v.strftime("%Y-%m-%d")
+        for k, v in loc_monitor_params.items()
+    }
+    (save_folder / "monitor_params.json").write_text(
+        json.dumps(moitor_params, indent=4)
+    )
+
+    # save dates
+    (save_folder / "dates.json").write_text(
+        json.dumps([d.strftime("%Y-%m-%d") for d in dates], indent=4)
+    )
+
+    # save data
+    np.save((save_folder / "data.npy"), data)
+
     return
+
 
 def break_to_decimal_year(idx, dates):
     """
     break dates of change into decimal year
     build to be vectorized over the resulting bfast break events
-    using the following format 
+    using the following format
     """
-    
-    # everything could have been done in a lambda function but for the sake of clarity I prefer to use it 
+
+    # everything could have been done in a lambda function but for the sake of clarity I prefer to use it
     if idx < 0:
         return np.nan
     else:
-        break_date = dates[idx-1]
-        return break_date.year + (break_date.timetuple().tm_yday - 1)/365
+        break_date = dates[idx - 1]
+        return break_date.year + (break_date.timetuple().tm_yday - 1) / 365
 
-def bfast_window(window, read_lock, write_lock, src, dst, segment_dir, monitor_params, crop_params, out):
+
+def bfast_window(
+    window,
+    read_lock,
+    write_lock,
+    src,
+    dst,
+    segment_dir,
+    monitor_params,
+    crop_params,
+    out,
+):
     """Run the bfast model on image windows"""
-    
+
     # read in a read_lock to avoid duplicate reading and corruption of the data
     with read_lock:
         data = src.read(window=window).astype(np.int16)
         # all the nan are transformed into 0 by casting don't we want to use np.iinfo.minint16 ?
-    
+
     # read the local observation date
-    with (segment_dir/'dates.csv').open() as f:
-        dates = sorted([dt.strptime(l, "%Y-%m-%d") for l in f.read().splitlines() if l.rstrip()])
-        
-    # update the crop and bfast params with the current tile dates 
-    crop_params = {k: next(d for d in dates if d > val) for k, val in crop_params.items()}
-    loc_monitor_params = {**monitor_params, 'start_monitor': next(d for d in dates if d > monitor_params['start_monitor'])}
-        
+    with (segment_dir / "dates.csv").open() as f:
+        dates = sorted(
+            [dt.strptime(l, "%Y-%m-%d") for l in f.read().splitlines() if l.rstrip()]
+        )
+
+    # update the crop and bfast params with the current tile dates
+    crop_params = {
+        k: next(d for d in dates if d > val) for k, val in crop_params.items()
+    }
+    loc_monitor_params = {
+        **monitor_params,
+        "start_monitor": next(d for d in dates if d > monitor_params["start_monitor"]),
+    }
+
     # crop the initial data to the used dates
-    data, dates = crop_data_dates(data,  dates, **crop_params)
-    
+    data, dates = crop_data_dates(data, dates, **crop_params)
+
     # start the bfast process
     model = BFASTMonitor(**loc_monitor_params)
-        
-    # fit the model 
+
+    # fit the model
     model.fit(data, dates)
 
     # vectorized fonction to format the results as decimal year (e.g mid 2015 will be 2015.5)
     to_decimal = np.vectorize(break_to_decimal_year, excluded=[1])
-    
+
     # slice the date to narrow it to the monitoring dates
-    start = loc_monitor_params['start_monitor']
-    end = crop_params['end']
-    monitoring_dates = dates[dates.index(start):dates.index(end)+1] # carreful slicing is x in [i,j[ 
-    
-    # compute the decimal break on the model 
+    start = loc_monitor_params["start_monitor"]
+    end = crop_params["end"]
+    monitoring_dates = dates[
+        dates.index(start) : dates.index(end) + 1
+    ]  # carreful slicing is x in [i,j[
+
+    # compute the decimal break on the model
     decimal_breaks = to_decimal(model.breaks, monitoring_dates)
-    
+
     # agregate the results on 2 bands
-    monitoring_results = np.stack((decimal_breaks, model.magnitudes, model.valids)).astype(np.float32)
-    
+    monitoring_results = np.stack(
+        (decimal_breaks, model.magnitudes, model.valids)
+    ).astype(np.float32)
+
     with write_lock:
         dst.write(monitoring_results, window=window)
         out.update_progress()
-        
+
     return
-        
-def run_bfast(folder, out_dir, tiles, monitoring, history, freq, k, hfrac, trend, level, backend, out):
+
+
+def run_bfast(
+    folder,
+    out_dir,
+    tiles,
+    monitoring,
+    history,
+    freq,
+    k,
+    hfrac,
+    trend,
+    level,
+    backend,
+    out,
+):
     """pilot the different threads that will launch the bfast process on windows"""
-    
-    # prepare parameters for crop as a dict 
-    crop_params = {
-        'start': history,
-        'end': monitoring[1]
-    }
-        
-    # prepare parameters for the bfastmonitor function 
+
+    # prepare parameters for crop as a dict
+    crop_params = {"start": history, "end": monitoring[1]}
+
+    # prepare parameters for the bfastmonitor function
     monitor_params = {
-        'start_monitor': monitoring[0],
-        'freq': freq,
-        'k': k,
-        'hfrac': hfrac,
-        'trend': trend,
-        'level': 1-level,  # it's an hidden parameter I hate it https://github.com/diku-dk/bfast/issues/23
-        'backend': backend
+        "start_monitor": monitoring[0],
+        "freq": freq,
+        "k": k,
+        "hfrac": hfrac,
+        "trend": trend,
+        "level": 1
+        - level,  # it's an hidden parameter I hate it https://github.com/diku-dk/bfast/issues/23
+        "backend": backend,
     }
-    
+
     # create 1 folder for each set of parameter
-    parameter_string = f'{history.year}_{monitoring[0].year}_{monitoring[1].year}_k{k}_f{freq}_t{int(trend)}_h{hfrac}_l{level}'
-    save_dir = cp.result_dir/out_dir/parameter_string
+    parameter_string = f"{history.year}_{monitoring[0].year}_{monitoring[1].year}_k{k}_f{freq}_t{int(trend)}_h{hfrac}_l{level}"
+    save_dir = cp.result_dir / out_dir / parameter_string
     save_dir.mkdir(parents=True, exist_ok=True)
-    
+
     # loop through the tiles
     file_list = []
     for tile in tiles:
-        
-        # get the starting time 
+
+        # get the starting time
         start = dt.now()
-        
-        # get the segment useful folders 
-        tile_dir = folder/tile
-        tile_save_dir = save_dir/tile
+
+        # get the segment useful folders
+        tile_dir = folder / tile
+        tile_save_dir = save_dir / tile
         tile_save_dir.mkdir(exist_ok=True)
-        
+
         # set the log and output file names
-        log_file = tile_save_dir/f'tile_{tile}.log'
-        file = tile_save_dir/'bfast_outputs.tif'
-        
-        # check the logs to see if the tile is already finished 
+        log_file = tile_save_dir / f"tile_{tile}.log"
+        file = tile_save_dir / "bfast_outputs.tif"
+
+        # check the logs to see if the tile is already finished
         if log_file.is_file():
             out.add_msg(cm.bfast.skip.format(tile))
-            time.sleep(.5) # to let people read the message
+            time.sleep(0.5)  # to let people read the message
             file_list.append(str(file))
             continue
-        
+
         # create the locks to avoid data coruption
         read_lock = threading.Lock()
         write_lock = threading.Lock()
 
         # get the profile from the master vrt
-        with rio.open(tile_dir/'stack.vrt', GEOREF_SOURCES='INTERNAL') as src:
-            
+        with rio.open(tile_dir / "stack.vrt", GEOREF_SOURCES="INTERNAL") as src:
+
             windows_size = 1024
-            
+
             profile = src.profile.copy()
             profile.update(
-                blockxsize = windows_size, 
-                blockysize = windows_size,
-                driver = 'GTiff',
-                count = 3,
-                dtype = np.float32
+                blockxsize=windows_size,
+                blockysize=windows_size,
+                driver="GTiff",
+                count=3,
+                dtype=np.float32,
             )
-            
-            # execute the concurent threads and write the results in a dst file 
-            with rio.open(file, 'w', **profile) as dst:
-                
+
+            # execute the concurent threads and write the results in a dst file
+            with rio.open(file, "w", **profile) as dst:
+
                 # get the windows
                 windows = [w for _, w in dst.block_windows()]
 
                 # display an tile computation message
                 out.add_live_msg(cm.bfast.sum_up.format(len(windows), tile))
 
-                # reset the output 
+                # reset the output
                 out.reset_progress(len(windows), cm.bfast.progress.format(tile))
-                
+
                 bfast_params = {
-                    'read_lock': read_lock, 
-                    'write_lock': write_lock,
-                    'src': src,
-                    'dst': dst,
-                    'segment_dir': tile_dir, 
-                    'monitor_params': monitor_params, 
-                    'crop_params': crop_params,
-                    'out': out
+                    "read_lock": read_lock,
+                    "write_lock": write_lock,
+                    "src": src,
+                    "dst": dst,
+                    "segment_dir": tile_dir,
+                    "monitor_params": monitor_params,
+                    "crop_params": crop_params,
+                    "out": out,
                 }
-                
+
                 # test outside the future
-                #for window in windows:
-                #    bfast_window(window, **bfast_params)
-                #    raise Exception ("done")
-                
-                with futures.ThreadPoolExecutor() as executor: # use all the available CPU/GPU
-                    executor.map(partial(bfast_window, **bfast_params), windows)
-        
+                for window in windows:
+                    bfast_window(window, **bfast_params)
+
+                # with futures.ThreadPoolExecutor() as executor: # use all the available CPU/GPU
+                #    executor.map(partial(bfast_window, **bfast_params), windows)
+
         # write in the logs that the tile is finished
         write_logs(log_file, start, dt.now())
-        
+
         # add the file to the file_list
         file_list.append(str(file))
-        
+
     # write a global vrt file to open all the tile at once
-    vrt_path = save_dir/f'bfast_outputs_{out_dir}.vrt'
+    vrt_path = save_dir / f"bfast_outputs_{out_dir}.vrt"
     ds = gdal.BuildVRT(str(vrt_path), file_list)
     ds.FlushCache()
-        
+
     # check that the file was effectively created (gdal doesn't raise errors)
     if not vrt_path.is_file():
         raise Exception(f"the vrt {vrt_path} was not created")
-    
-    # return a str for the traitlets 
+
+    # return a str for the traitlets
     return str(save_dir)
 
+
 def write_logs(log_file, start, end):
-    
-    with log_file.open('w') as f: 
+
+    with log_file.open("w") as f:
         f.write("Computation finished!\n")
         f.write("\n")
         f.write(f"Computation started on: {start} \n")
         f.write(f"Computation finished on: {end}\n")
         f.write("\n")
         f.write(f"Elapsed time: {end-start}")
-        
+
     return
-    
-    
-        
-        
-        
--- component/tile/result_tile.py
+++ component/tile/result_tile.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from sepal_ui import sepalwidgets as sw 
+from sepal_ui import sepalwidgets as sw
 from sepal_ui import mapping as sm
 from sepal_ui.scripts.utils import loading_button
 import ipyvuetify as v
@@ -12,68 +12,68 @@ import rasterio as rio
 from component import parameter as cp
 from component.message import cm
 
+
 class ResultTile(sw.Tile):
-    
+
     out_dir = Any(None).tag(sync=True)
-    
+
     def __init__(self, bfast_tile):
-        
+
         # create the map
         self.m = sm.SepalMap()
-        self.m.add_legend( 
-            legend_title = cm.display.legend,
+        self.m.add_legend(
+            legend_title=cm.display.legend,
             legend_dict={k: c for k, c in cp.legend.values()},
-            position='topleft'
+            position="topleft",
         )
-        
+
         super().__init__(
-            'result_tile',
+            "result_tile",
             cm.display.title,
-            inputs = [v.Flex(class_='mt-5 mb-5', children=[self.m])],
-            btn = sw.Btn(cm.display.btn),
-            alert = sw.Alert()
+            inputs=[v.Flex(class_="mt-5 mb-5", children=[self.m])],
+            btn=sw.Btn(cm.display.btn),
+            alert=sw.Alert(),
         )
-        
-        # js events 
-        bfast_tile.observe(self._change_dir, 'dir_')
-        self.btn.on_event('click', self._compute_map)
-        
+
+        # js events
+        bfast_tile.observe(self._change_dir, "dir_")
+        self.btn.on_event("click", self._compute_map)
+
     def _change_dir(self, change):
-        
-        self.out_dir = Path(change['new'])
-        
+
+        self.out_dir = change["new"]
+
         return self
-        
-    @loading_button(debug=False)
+
+    @loading_button(debug=True)
     def _compute_map(self, widget, event, data):
         """compute the threshold data map and display it on the interactive map"""
-        
-        if not self.out_dir: raise Exception(cm.display.no_bfast)
-        
+
+        if not self.out_dir:
+            raise Exception(cm.display.no_bfast)
+
         # check if the bfast_output vrt exist
-        bfast_output = self.out_dir/f'bfast_outputs_{self.out_dir.parent.stem}.vrt'
-        
-        if not bfast_output.is_file(): raise Exception(cm.display.no_bfast)
-        
+        out_dir = Path(self.out_dir)
+        bfast_output = out_dir / f"bfast_outputs_{out_dir.parent.stem}.vrt"
+
+        if not bfast_output.is_file():
+            raise Exception(cm.display.no_bfast)
+
         # check if the threshold map exist
-        threshold_output = bfast_output.parent/f'threshold_{self.out_dir.parent.stem}.tif'
+        threshold_output = bfast_output.parent / f"threshold_{out_dir.parent.stem}.tif"
         if not threshold_output.is_dir():
-            
-            with rio.open(bfast_output, GEOREF_SOURCES='INTERNAL') as src:
-                
-                # get the profile 
+
+            with rio.open(bfast_output, GEOREF_SOURCES="INTERNAL") as src:
+
+                # get the profile
                 profile = src.profile.copy()
-                profile.update(
-                    driver = 'GTiff',
-                    count = 1,
-                    dtype = np.int8
-                )
-                
+                profile.update(driver="GTiff", count=1, dtype=np.int8)
+
                 data = src.read(2).astype(np.int16)
-                
+
                 std = data.flatten().std()
                 mean = data.flatten().mean()
-                
+
                 final_data = (
                     (data > (mean + (std * 4))) * 9
                     + (data <= (mean + (std * 4))) * (data > (mean + (std * 3))) * 8
@@ -84,19 +84,18 @@ class ResultTile(sw.Tile):
                     + (data <= (mean - std)) * (data > (mean - (std * 2))) * 2
                     + (data <= (mean - (std * 2))) * (data > (mean - (std * 3))) * 3
                     + (data <= (mean - (std * 3))) * (data > (mean - (std * 4))) * 4
-                    + (data <= (mean - (std * 4))) * 5      
-                )           
-                    
-                with rio.open(threshold_output, 'w', **profile) as dst:
+                    + (data <= (mean - (std * 4))) * 5
+                )
+
+                with rio.open(threshold_output, "w", **profile) as dst:
                     dst.write(final_data.astype(np.int8), 1)
-            
-        # display it in the interactive map        
+
+        # display it in the interactive map
         self.m.add_raster(
-            threshold_output, 
-            layer_name=cm.display.layer, 
-            colormap=ListedColormap([cp.legend[i][1] for i in sorted(cp.legend)]),  
-            colorbar_position=False
+            threshold_output,
+            layer_name=cm.display.layer,
+            colormap=ListedColormap([cp.legend[i][1] for i in sorted(cp.legend)]),
+            colorbar_position=False,
         )
-        
+
         return self
-        
